998650cf3cda49f3bf20f31bebcf7dba570f3298
ZwangaMukwevho <zwangatm3@gmail.com>
2023-03-19 03:03:11 +0200

    fix: finished up step progress bar

--- components/upload/dragAndDrop.js
+++ components/upload/dragAndDrop.js
@@ -15,6 +15,17 @@ import {
   updateMetadata,
 } from "firebase/storage";
 
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import StepConnector, {
+  stepConnectorClasses,
+} from "@mui/material/StepConnector";
+import { styled } from "@mui/material/styles";
+
 const baseStyle = {
   display: "flex",
   flexDirection: "column",
@@ -40,6 +51,12 @@ const acceptStyle = {
 const rejectStyle = {
   borderColor: "#ff1744",
 };
+
+const steps = [
+  "upload picture of yourself holding an your id",
+  "upload document to certify",
+  "Submit",
+];
 
 function DropzoneComponent(props) {
   const [error, setError] = useState("");
@@ -110,19 +127,6 @@ function DropzoneComponent(props) {
   );
 
   const prevCount = useRef(0);
-  function step1Validator() {
-    if (prevCount.current >= 1) {
-      return true;
-    }
-    return false;
-  }
-
-  function step2Validator() {
-    if (prevCount.current >= 2) {
-      return true;
-    }
-    return false;
-  }
 
   function onFormSubmit() {
     setLoader(true);
@@ -158,6 +162,87 @@ function DropzoneComponent(props) {
     prevCount.current = docsToUploadArr.length;
   }, [docsToUploadArr]);
 
+  // Step progress bar logic
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set());
+
+  const isStepOptional = (step) => {
+    return step === 20;
+  };
+
+  const isStepSkipped = (step) => {
+    return skipped.has(step);
+  };
+
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleSkip = () => {
+    if (!isStepOptional(activeStep)) {
+      // You probably want to guard against something like this,
+      // it should never occur unless someone's actively trying to break something.
+      throw new Error("You can't skip a step that isn't optional.");
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped((prevSkipped) => {
+      const newSkipped = new Set(prevSkipped.values());
+      newSkipped.add(activeStep);
+      return newSkipped;
+    });
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+    onFormSubmit();
+  };
+
+  const QontoConnector = styled(StepConnector)(({ theme }) => ({
+    [`&.${stepConnectorClasses.alternativeLabel}`]: {
+      top: 10,
+      left: "calc(-50% + 16px)",
+      right: "calc(50% + 16px)",
+    },
+    [`&.${stepConnectorClasses.active}`]: {
+      [`& .${stepConnectorClasses.line}`]: {
+        borderColor: "#784af4",
+      },
+    },
+    [`&.${stepConnectorClasses.completed}`]: {
+      [`& .${stepConnectorClasses.line}`]: {
+        borderColor: "#784af4",
+      },
+    },
+    [`& .${stepConnectorClasses.line}`]: {
+      borderColor:
+        theme.palette.mode === "dark" ? theme.palette.grey[800] : "#eaeaf0",
+      borderTopWidth: 3,
+      borderRadius: 1,
+    },
+  }));
+
+  const isStepFailed = (step) => {
+    if (prevCount.current === step && step !== 2) {
+      return true;
+    }
+    return false;
+  };
+
+  console.log("active step");
+  console.log(activeStep);
+  console.log(steps.length);
   return (
     <>
       <div className={classes.loader}>
@@ -182,7 +267,91 @@ function DropzoneComponent(props) {
       </div>
       <DocsToUpload />
       <div className={classes.margin}>
-        <HorizontalLinearStepper docCount={prevCount.current} />
+        <Box sx={{ width: "100%" }}>
+          <Stepper
+            activeStep={activeStep}
+            alternativeLabel
+            connector={<QontoConnector />}
+          >
+            {steps.map((label, index) => {
+              const stepProps = {};
+              const labelProps = {};
+
+              if (isStepFailed(index)) {
+                labelProps.optional = (
+                  <Typography variant="caption" color="error">
+                    upload document
+                  </Typography>
+                );
+
+                labelProps.error = true;
+              }
+
+              return (
+                <Step key={label} {...stepProps}>
+                  <StepLabel {...labelProps}>
+                    <div className={classes.stepperLabel}>{label}</div>
+                  </StepLabel>
+                </Step>
+              );
+            })}
+          </Stepper>
+          {activeStep === steps.length - 1 ? (
+            <React.Fragment>
+              <Typography sx={{ mt: 2, mb: 1 }}>
+                All documents uploaded - click submit
+              </Typography>
+              <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                <Button
+                  color="inherit"
+                  disabled={activeStep === 0}
+                  onClick={handleBack}
+                  sx={{ mr: 1 }}
+                  variant="contained"
+                >
+                  Back
+                </Button>
+                <Box sx={{ flex: "1 1 auto" }} />
+                <Button variant="outlined" onClick={handleReset}>
+                  Submit
+                </Button>
+              </Box>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <Typography sx={{ mt: 2, mb: 1 }}>
+                click step done to activate next button
+              </Typography>
+              <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                <Button
+                  color="inherit"
+                  disabled={activeStep === 0}
+                  onClick={handleBack}
+                  sx={{ mr: 1 }}
+                  variant="contained"
+                >
+                  Back
+                </Button>
+                <Box sx={{ flex: "1 1 auto" }} />
+                {isStepOptional(activeStep) && (
+                  <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
+                    Skip
+                  </Button>
+                )}
+                {prevCount.current <= activeStep ? (
+                  <Button variant="contained" onClick={handleNext} disabled>
+                    {activeStep === steps.length - 1 ? "Submit" : "Next"}
+                  </Button>
+                ) : (
+                  <Button variant="contained" onClick={handleNext}>
+                    {activeStep === steps.length - 1 ? "Submit" : "Next"}
+                  </Button>
+                )}
+              </Box>
+            </React.Fragment>
+          )}
+        </Box>
+        <Button variant="contained">step done</Button>
       </div>
     </>
   );
