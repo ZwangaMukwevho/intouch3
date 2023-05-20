25566ef46346e567d314bdc822b8ab240af1e576
ZwangaMukwevho <zwangatm3@gmail.com>
2023-05-21 00:52:18 +0200

    fix: fixed conditional use of useRe

--- components/content/rowItemV2.js
+++ components/content/rowItemV2.js
@@ -1,61 +1,71 @@
 import classes from "./rowItem.module.css";
 import { formatDate, getStatus } from "../../logic/data/format";
 import { statusStyle } from "../../logic/status";
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 import { download } from "../../logic/download.js";
 
 export default function RowItemV2(props) {
   const status = getStatus(props.status);
-  if (typeof window !== "undefined") {
-    const windowSize = useRef([window.innerWidth, window.innerHeight]);
-    var width = windowSize.current[0];
-  } else {
-    var width = 500;
-  }
+  const [width, setWidth] = useState(window.innerWidth);
 
-  if (width <= 480) {
-    return (
-      <>
-        <div className={classes.row}>
-          <div id={classes.title}>
-            <div>{props.title}</div>
-          </div>
-          <div id={classes.type}>
-            <div>{props.type}</div>
-          </div>
-          <div id={classes.date}>
-            <div>{formatDate(props.date)}</div>
-          </div>
-          <div id={classes.status}>
-            <div className={statusStyle(status)}>{status}</div>
-          </div>
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    // Update windowSize on initial mount
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  console.log(width);
+
+  return width <= 480 ? (
+    <>
+      <div className={classes.row}>
+        <div id={classes.title}>
+          <div>{props.title}</div>
         </div>
-        <div className={classes.downloadRow}>
+        <div id={classes.type}>
+          <div>{props.type}</div>
+        </div>
+        <div id={classes.date}>
+          <div>{formatDate(props.date)}</div>
+        </div>
+        <div id={classes.status}>
+          <div className={statusStyle(status)}>{status}</div>
+        </div>
+      </div>
+      <div className={classes.downloadRow}>
+        <div>{download(status, props.docRef, props.title, props.id)}</div>
+      </div>
+    </>
+  ) : (
+    <>
+      <div className={classes.row}>
+        <div id={classes.title}>
+          <div>{props.title}</div>
+        </div>
+        <div id={classes.type}>
+          <div>{props.type}</div>
+        </div>
+        <div id={classes.date}>
+          <div>{formatDate(props.date)}</div>
+        </div>
+        <div id={classes.status}>
+          <div className={statusStyle(status)}>{status}</div>
+        </div>
+        <div id={classes.download}>
           <div>{download(status, props.docRef, props.title, props.id)}</div>
         </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div className={classes.row}>
-          <div id={classes.title}>
-            <div>{props.title}</div>
-          </div>
-          <div id={classes.type}>
-            <div>{props.type}</div>
-          </div>
-          <div id={classes.date}>
-            <div>{formatDate(props.date)}</div>
-          </div>
-          <div id={classes.status}>
-            <div className={statusStyle(status)}>{status}</div>
-          </div>
-          <div id={classes.download}>
-            <div>{download(status, props.docRef, props.title, props.id)}</div>
-          </div>
-        </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
